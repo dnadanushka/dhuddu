58f999adc4f2b0ae3c65f1e73bf2698b3cf7aa90
Danushka <dnadanushka>
2020-07-21 22:13:08 +0530

    facebook login added

--- lib/screens/start.dart
+++ lib/screens/start.dart
@@ -1,8 +1,52 @@
 import 'package:dhuddu/util/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
 
+class Start extends StatefulWidget {
+  @override
+  _StartState createState() => _StartState();
+}
 
-class Start extends StatelessWidget {
+class _StartState extends State<Start> {
+  bool _isLoggedIn = false;
+  Map userProfile;
+
+  final facebookLogin = FacebookLogin();
+
+  _loginWithFB() async {
+    final result = await facebookLogin.logIn(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userProfile = profile;
+          _isLoggedIn = true;
+        });
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false);
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false);
+        break;
+    }
+  }
+
+  _logout() {
+    facebookLogin.logOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -11,14 +55,18 @@ class Start extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        SizedBox(height: SizeConfig.blockSizeVertical*10,),
+        SizedBox(
+          height: SizeConfig.blockSizeVertical * 10,
+        ),
         SizedBox(
           height: SizeConfig.blockSizeVertical * 15,
           child: Image.asset(
             'assets/logo.png',
           ),
         ),
-          SizedBox(height: SizeConfig.blockSizeVertical*8,),
+        SizedBox(
+          height: SizeConfig.blockSizeVertical * 8,
+        ),
         Text(
           'Signup Here',
           style: TextStyle(
@@ -31,7 +79,9 @@ class Start extends StatelessWidget {
             fontSize: SizeConfig.blockSizeHorizontal * 4,
           ),
         ),
-          SizedBox(height: SizeConfig.blockSizeVertical*5,),
+        SizedBox(
+          height: SizeConfig.blockSizeVertical * 5,
+        ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
@@ -39,7 +89,9 @@ class Start extends StatelessWidget {
             children: <Widget>[
               FloatingActionButton(
                 backgroundColor: Colors.white,
-                onPressed: null,
+                onPressed: () {
+                  _loginWithFB();
+                },
                 child: Image.asset(
                   'assets/facebook.png',
                 ),
@@ -62,7 +114,25 @@ class Start extends StatelessWidget {
             ],
           ),
         ),
-
+        _isLoggedIn
+            ? Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Image.network(
+                    userProfile["picture"]["data"]["url"],
+                    height: 50.0,
+                    width: 50.0,
+                  ),
+                  Text(userProfile["name"]),
+                  OutlineButton(
+                    onPressed: () {
+                      _logout();
+                    },
+                    child: Text('Logout'),
+                  )
+                ],
+              )
+            : Container(),
       ],
     ));
   }
