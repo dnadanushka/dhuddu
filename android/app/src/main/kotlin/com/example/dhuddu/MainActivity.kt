1bb63aabb8b0cef9d13c5dcd69d772af370d2be9
Danushka <dnadanushka>
2020-07-21 15:43:58 +0530

    Innitial commit

---
+++ android/app/src/main/kotlin/com/example/dhuddu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dhuddu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/start.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Dhuddu'),
+      ),
+    );
+  }
+}
